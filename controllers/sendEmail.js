e4ec0f80ed1aa7ec1df24c587dd6e91b15441b7a
Edmond <kzwrdmnd@gmail.com>
2025-04-22 11:57:23 +0200

    ed

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -10,7 +10,7 @@ const nodemailer = require('nodemailer');
 async function sendEmail(to, subject, text) {
   try {
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      service: 'gmailfgfg',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
